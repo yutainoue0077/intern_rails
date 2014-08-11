36e2cb68a08ddd9adba6a16adb7be169326c0f71
yutainoue <yutainoue009@gmail.com>
2014-08-11 16:19:53 +0900

    第三章終了

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,36 +1,51 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+    let(:base_title) { "Sample App | " }
+    let(:v_home) {visit '/static_pages/home'}
+    let(:v_help) {visit '/static_pages/help'}
+    let(:v_about) {visit '/static_pages/about'}
+    let(:v_contact) {visit '/static_pages/contact'}
   describe "Home page" do
 
     it "/home には 'ほーむ' という言葉が含まれる" do
-      visit '/static_pages/home'
+      v_home
       expect(page).to have_content('ほーむ')
     end
 
     it "/home は以下のtitleを保有する" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Sample App | ほーむ")
+      v_home
+      expect(page).to have_title("#{base_title}ほーむ")
     end
 
     it "/help には 'へるぷ' という言葉が含まれる" do
-      visit '/static_pages/help'
+      v_help
       expect(page).to have_content('へるぷ')
     end
 
     it "/help は以下のtitleを保有する" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Sample App | へるぷ")
+      v_help
+      expect(page).to have_title("#{base_title}へるぷ")
     end
 
     it "/about には 'あばうと' という言葉が含まれる" do
-      visit '/static_pages/about'
+      v_about
       expect(page).to have_content('あばうと')
     end
 
     it "/about は以下のtitleを保有する" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Sample App | あばうと")
+      v_about
+      expect(page).to have_title("#{base_title}あばうと")
+    end
+
+    it "/contact には 'こんたくと' という言葉が含まれる" do
+      v_contact
+      expect(page).to have_content('こんたくと')
+    end
+
+    it "/contact は以下のtitleを保有する" do
+      v_contact
+      expect(page).to have_title("Sample App | こんたくと")
     end
   end
 end
