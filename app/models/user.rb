400cf4cc0b82c53c146ee055dc0fb47bb537d1cc
yutainoue <inoue_yuta@nexway.co.jp>
2014-08-22 16:23:35 +0900

    11章インターフェイス制作前

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,41 +1,59 @@
 class User < ActiveRecord::Base
-    has_many :microposts, dependent: :destroy
+  has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-    before_save { email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  before_save { email.downcase! }
 
-    before_create (:create_remember_token)
-    validates(:name, presence: true, length: {maximum:30})
-    validates(:email, presence: true, format: { with: VALID_EMAIL_REGEX },
-               uniqueness:{ case_sensitive: false })
-    validates(:password, length: {minimum: 6})
+  before_create (:create_remember_token)
+  validates(:name, presence: true, length: {maximum:30})
+  validates(:email, presence: true, format: { with: VALID_EMAIL_REGEX },
+             uniqueness:{ case_sensitive: false })
+  validates(:password, length: {minimum: 6})
 
-    has_secure_password
-
-
-
-    ###############################
+  has_secure_password
 
 
-    def feed
-      # このコードは準備段階です。
-      # 完全な実装は第11章「ユーザーをフォローする」を参照してください。
-      Micropost.where("user_id = ?", id)
-    end
 
-    def User.new_remember_token
-      SecureRandom.urlsafe_base64
-    end
+  ###############################
 
-    def User.encrypt(token)
-      Digest::SHA1.hexdigest(token.to_s)
-    end
 
-    private
+  def feed
+    # このコードは準備段階です。
+    # 完全な実装は第11章「ユーザーをフォローする」を参照してください。
+    Micropost.where("user_id = ?", id)
+  end
 
-    def create_remember_token
-      self.remember_token = User.encrypt(User.new_remember_token)
-    end
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    self.relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 
 
 end
